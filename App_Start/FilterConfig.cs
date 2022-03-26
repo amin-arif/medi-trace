97edff7c67c63079f54d97dfce1a9c0786d4173d
arif <mdarifkanon@gmail.com>
2022-03-26 09:00:50 +0600

    Initial commit

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace medi_trace
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
